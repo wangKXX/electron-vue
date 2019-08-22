7b6b53f304bb1485b80222370e597e7146a7814d
kai.wang <1944821630@example.com>
2019-08-22 16:57:21 +0800

    [feat]将用户信息提交到后台

--- src/renderer/components/friend-list/addTask.vue
+++ src/renderer/components/friend-list/addTask.vue
@@ -25,8 +25,8 @@ export default {
     handlerClick() {
       const mesgContent = {
         mesg: {
-          re: { id: this.user.id },
-          user: { id: this.userInfo.id },
+          re: { ...this.user.id }, // 自己
+          user: { ...this.userInfo.id }, // 目标对象
           type: "re_add"
         },
         type: "re_add"
